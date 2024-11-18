4ff8c7f29695e2e55ecb1c631d69362ebd5f596d
jay <jaycantemprate31@gmail.com>
2024-11-18 22:36:30 +0800

    change

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -4,64 +4,40 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Simple Laravel 11 CRUD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
-
-    <div class="bg-dark py-3">
-        <h3 class="text-white text-center">Simple Laravel 11 CRUD</h3>
-    </div>
     <div class="container">
-        <div class="row justify-content-center mt-4">
-            <div class="col-md-10 d-flex justify-content-end">
-                <a href="{{ route('products.index') }}" class="btn btn-dark">Back</a>
-            </div>
-        </div>
         <div class="row d-flex justify-content-center">
             <div class="col-md-10">
                 <div class="card border-0 shadow-lg my-4">
                     <div class="card-header bg-dark">
-                        <h3 class="text-white">Create Product</h3>
+                        <h3 class="text-white">Add Product</h3>
                     </div>
                     <form enctype="multipart/form-data" action="{{ route('products.store') }}" method="POST">
                         @csrf
                         <div class="card-body">
                             <div class="mb-3">
                                 <label for="name" class="form-label h5">Name</label>
-                                <input id="name" type="text" name="name" value="{{ old('name') }}"
-                                    class="form-control form-control-lg @error('name') is-invalid @enderror"
-                                    placeholder="Name">
-                                @error('name')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
+                                <input id="name" type="text" name="name" class="form-control form-control-lg"
+                                    placeholder="Name" required>
                             </div>
                             <div class="mb-3">
                                 <label for="price" class="form-label h5">Price</label>
-                                <input id="price" type="text" name="price" value="{{ old('price') }}"
-                                    class="form-control form-control-lg @error('price') is-invalid @enderror"
+                                <input id="price" type="text" name="price" class="form-control form-control-lg"
                                     placeholder="Price">
-                                @error('price')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="description" class="form-label h5">Description</label>
-                                <textarea id="description" name="description" cols="30" rows="5"
-                                    class="form-control @error('description') is-invalid @enderror" placeholder="Description">{{ old('description') }}</textarea>
-                                @error('description')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
+                                <textarea id="description" name="description" cols="30" rows="5" class="form-control"
+                                    placeholder="Description"></textarea>
                             </div>
                             <div class="mb-3">
                                 <label for="image" class="form-label h5">Product Image</label>
                                 <input id="image" type="file" name="image"
-                                    class="form-control form-control-lg @error('image') is-invalid @enderror">
-                                @error('image')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
+                                    class="form-control form-control-lg">
                             </div>
                             <div class="d-grid">
                                 <button type="submit" class="btn btn-lg btn-primary">Submit</button>
@@ -69,6 +45,11 @@
                         </div>
                     </form>
                 </div>
+            </div>
+        </div>
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-10 d-flex justify-content-end">
+                <a href="{{ route('products.index') }}" class="btn btn-dark">Back</a>
             </div>
         </div>
     </div>
--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -4,15 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Simple Laravel 11 CRUD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="bg-dark py-3">
-        <h3 class="text-white text-center">Simple Laravel 11 CRUD</h3>
-    </div>
     <div class="container">
         <div class="row justify-content-center mt-4">
             <div class="col-md-10 d-flex justify-content-end">
@@ -20,13 +16,6 @@
             </div>
         </div>
         <div class="row d-flex justify-content-center">
-            @if (Session::has('success'))
-                <div class="col-md-10 mt-4">
-                    <div class="alert alert-success">
-                        {{ Session::get('success') }}
-                    </div>
-                </div>
-            @endif
             <div class="col-md-10">
                 <div class="card borde-0 shadow-lg my-4">
                     <div class="card-header bg-dark">
@@ -42,39 +31,34 @@
                                 <th>Created at</th>
                                 <th>Action</th>
                             </tr>
-                            @if ($products->isNotEmpty())
-                                @foreach ($products as $product)
-                                    <tr>
-                                        <td>{{ $product->id }}</td>
-                                        <td>
-                                            @if ($product->image != '')
-                                                <img width="50"
-                                                    src="{{ asset('uploads/products/' . $product->image) }}"
-                                                    alt="">
-                                            @endif
-                                        </td>
-                                        <td>{{ $product->name }}</td>
-                                        <td>${{ $product->price }}</td>
-                                        <td>{{ \Carbon\Carbon::parse($product->created_at)->format('d M, Y') }}</td>
-                                        <td>
-                                            <a href="{{ route('products.edit', $product->id) }}"
-                                                class="btn btn-dark">Edit</a>
-                                            <a href="#" onclick="deleteProduct({{ $product->id }});"
-                                                class="btn btn-danger">Delete</a>
-                                            <form id="delete-product-from-{{ $product->id }}"
-                                                action="{{ route('products.destroy', $product->id) }}" method="post">
-                                                @csrf
-                                                @method('delete')
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-
+                            @foreach ($products as $product)
+                                <tr>
+                                    <td>{{ $product->id }}</td>
+                                    <td>
+                                        @if ($product->image != '')
+                                            <img width="50" src="{{ asset('uploads/products/' . $product->image) }}"
+                                                alt="">
+                                        @endif
+                                    </td>
+                                    <td>{{ $product->name }}</td>
+                                    <td>${{ $product->price }}</td>
+                                    <td>{{ \Carbon\Carbon::parse($product->created_at)->format('d M, Y') }}</td>
+                                    <td>
+                                        <a href="{{ route('products.edit', $product->id) }}"
+                                            class="btn btn-dark">Edit</a>
+                                        <a href="#" onclick="deleteProduct({{ $product->id }});"
+                                            class="btn btn-danger">Delete</a>
+                                        <form id="delete-product-from-{{ $product->id }}"
+                                            action="{{ route('products.destroy', $product->id) }}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
                             @endif
-
                         </table>
                     </div>
-
                 </div>
             </div>
         </div>
